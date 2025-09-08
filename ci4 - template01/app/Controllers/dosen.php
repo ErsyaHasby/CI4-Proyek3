e842f797536c0683854ebda75c395ca88a660d40
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-09-08 10:37:08 +0700

    add ci4-template

---
+++ ci4 - template01/app/Controllers/dosen.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class dosen extends Controller
+{
+    public function display(): string
+    {
+        return '<h1>Hello World!</h1>';
+    }
+}
+
---
+++ ci4 - template01/app/Views/berita.php
@@ -0,0 +1,2 @@
+<h3>Ini berita</h3>
+<p>Konten berita akan ditampilkan disini</p>
